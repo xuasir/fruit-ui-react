ebc0f76836d0a4b1e02f0e95e2ba315a7ac29234
xuguo <xuguo@leimingtech.com>
2020-03-29 19:37:05 +0800

    feat(app.tsx): 测试button组件样式
    对button组件各类样式的测试

--- src/app.tsx
+++ src/app.tsx
@@ -3,9 +3,22 @@ import Button, { ButtonSize, ButtonType } from "./components/Button/Button";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ padding: "10px" }}>
       <Button buttonType={ButtonType.Primary} size={ButtonSize.Large}>
-        hi
+        primary large
+      </Button>
+      <Button buttonType={ButtonType.Default}>default normal</Button>
+      <Button buttonType={ButtonType.Default} disabled>
+        default normal disabled
+      </Button>
+      <Button buttonType={ButtonType.Danger} size={ButtonSize.Small}>
+        danger small
+      </Button>
+      <Button buttonType={ButtonType.Link} href="http://www.baidu.com">
+        button link
+      </Button>
+      <Button buttonType={ButtonType.Link} href="http://www.baidu.com" disabled>
+        button link disabled
       </Button>
     </div>
   );
