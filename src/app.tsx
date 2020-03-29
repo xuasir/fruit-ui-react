3c5ac89e44ca6fb9e0fea637049ee50f263d866d
xuguo <xuguo@leimingtech.com>
2020-03-30 00:01:26 +0800

    feat(button.test): 为button组件添加单元测试
    集成jest,react-testing-library并为button组件添加四组单元测试

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,25 @@
 import React from "react";
-import Button, { ButtonSize, ButtonType } from "./components/Button/Button";
+import Button, {
+  ButtonSize,
+  ButtonType,
+  ButtonShape,
+} from "./components/Button/Button";
 
 function App() {
   return (
     <div className="App" style={{ padding: "10px" }}>
-      <Button buttonType={ButtonType.Primary} size={ButtonSize.Large}>
+      <Button
+        shape={ButtonShape.Round}
+        block
+        buttonType={ButtonType.Primary}
+        size={ButtonSize.Large}
+      >
         primary large
       </Button>
-      <Button buttonType={ButtonType.Default}>default normal</Button>
-      <Button buttonType={ButtonType.Default} disabled>
-        default normal disabled
+      <Button shape={ButtonShape.Round} buttonType={ButtonType.Default}>
+        default normal
       </Button>
+      <Button buttonType={ButtonType.Dashed}>default normal</Button>
       <Button buttonType={ButtonType.Danger} size={ButtonSize.Small}>
         danger small
       </Button>
---
+++ src/components/Button/Button.test.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import Button, {
+  ButtonShape,
+  ButtonSize,
+  ButtonType,
+  IButtonProps,
+} from "./Button";
+
+const defaultProps = {
+  onClick: jest.fn(),
+};
+
+const testProps: IButtonProps = {
+  buttonType: ButtonType.Primary,
+  size: ButtonSize.Large,
+  shape: ButtonShape.Round,
+  className: "fclass",
+};
+
+const disabledProps = {
+  onClick: jest.fn(),
+  disabled: true,
+};
+
+describe("test Button Compenent", () => {
+  it("should render the correct default button", () => {
+    const wrapper = render(<Button {...defaultProps}>Nice</Button>);
+    const element = wrapper.getByText("Nice");
+    expect(element).toBeInTheDocument();
+    expect(element.tagName).toEqual("BUTTON");
+    expect(element).toHaveClass("fx-btn fx-btn-default");
+    fireEvent.click(element);
+    expect(defaultProps.onClick).toHaveBeenCalled();
+  });
+  it("should render the correct component based on different props", () => {
+    const wrapper = render(<Button {...testProps}>Nice</Button>);
+    const element = wrapper.getByText("Nice");
+    expect(element).toBeInTheDocument();
+    expect(element).toHaveClass(
+      "fx-btn fx-btn-primary fx-btn-round-lg fx-btn-lg fclass"
+    );
+  });
+  it("should render a link when buttonType equals link and href is provided", () => {
+    const wrapper = render(
+      <Button buttonType={ButtonType.Link} href="http://www.baidu.com">
+        Nice
+      </Button>
+    );
+    const element = wrapper.getByText("Nice");
+    expect(element).toBeInTheDocument();
+    expect(element.tagName).toEqual("A");
+    expect(element).toHaveClass("fx-btn fx-btn-link");
+  });
+  it("should render disabled button when disabled set to true", () => {
+    const wrapper = render(<Button {...disabledProps}>Nice</Button>);
+    const element = wrapper.getByText("Nice") as HTMLButtonElement;
+    expect(element).toBeInTheDocument();
+    expect(element.tagName).toEqual("BUTTON");
+    expect(element).toHaveClass("fx-btn");
+    expect(element.disabled).toBeTruthy();
+    fireEvent.click(element);
+    expect(disabledProps.onClick).not.toHaveBeenCalled();
+  });
+});
