d8e7675183dd109a14e33ef98c55b9f6251d519f
xuguo <xg501@outlook.com>
2020-04-05 10:49:00 +0800

    feat(package): 集成storybook
    为项目集成storybook展示组件,并精简依赖

---
+++ .storybook/main.ts
@@ -0,0 +1,18 @@
+module.exports = {
+  stories: ['../src/**/*.stories.(tsx|mdx)'],
+  addons: [
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-docs'
+  ],
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      loader: require.resolve('babel-loader'),
+      options: {
+        presets: [['react-app', { flow: false, typescript: true }]],
+      },
+    });
+    config.resolve.extensions.push('.ts', '.tsx');
+    return config;
+  },
+};
---
+++ .storybook/preview.tsx
@@ -0,0 +1,8 @@
+import { addParameters } from '@storybook/react';
+import '../src/styles/index.scss'
+
+addParameters({
+  docs: {
+    inlineStories: false,
+  },
+});
